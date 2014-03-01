5f7ce3639a920fc79f981fcb46c5cc52d457f95e
abdellatifkarroum <abdellatifkarrom@gmail.com>
2014-03-01 00:20:18 +0000

    ajout de la sayHello

--- src/App/Bundle/BackOfficeBundle/Controller/DefaultController.php
+++ src/App/Bundle/BackOfficeBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Bundle\BackOfficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Bundle\BackOfficeBundle\Entity\Etudiant;
 
 class DefaultController extends Controller
 {
@@ -10,4 +11,8 @@ class DefaultController extends Controller
     {
         return $this->render('AppBackOfficeBundle:Default:index.html.twig', array('name' => $name));
     }
+	
+	private function seyHello(){
+		echo "hello e-scolarite";
+	}
 }
