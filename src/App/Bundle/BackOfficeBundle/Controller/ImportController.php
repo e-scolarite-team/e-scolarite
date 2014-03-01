95578091a60e89cd620358252e4ff7fce3685a75
fayssal tahtoub <fayssal.tahtoub@gmail.com>
2014-03-01 19:06:06 +0000

    add form import type and 3eme version of sql

--- src/App/Bundle/BackOfficeBundle/Controller/ImportController.php
+++ src/App/Bundle/BackOfficeBundle/Controller/ImportController.php
@@ -3,29 +3,115 @@
 namespace App\Bundle\BackOfficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Bundle\BackOfficeBundle\Entity\Element;
+use App\Bundle\BackOfficeBundle\Entity\Note;
+use App\Bundle\BackOfficeBundle\Entity\Module;
+use App\Bundle\BackOfficeBundle\Entity\Etudiant;
+
+use App\Bundle\BackOfficeBundle\Form\ImportFormType;
+
+use PHPExcel\PHPExcel;
+
+
 
 class ImportController extends Controller
 {
     public function updateAction()
     {
     	//abdelatif karoum todo here
-        return $this->render('AppBackOfficeBundle:Import:update.html.twig', array());
+        $form  = $this->createForm(new ImportFormType());
+        $status = $this->importElements('uploads/votre.xls');
+
+        if($status)
+            return new Response('table import!!');
+
+        return new Response('table error!!');
+
+        //return $this->render('AppBackOfficeBundle:Import:update.html.twig', array('form' => $form->createView()));
     }
 
-    private function importEtudiants(){
+    /**
+    * @param string
+    *
+    * @return boolean
+    */
+    private function importEtudiants($path){
     	//labied younes karoum todo here
+
+        $em = $this->get('doctrine')->getEntityManager();
+
+        //1er temps supression du content de la table etudiant
+
+        /** mettre ca dans la boucle d'insertion **/
+        //$etudiant = new Etudiant();
+        //$em->persist($etudiant);
+        //$em->flush();
+
+        return true;
     }
 
-    private function importNotes(){
+    /**
+    * @param string
+    *
+    * @return boolean
+    */
+    private function importNotes($path){
     	//paul todo here
+
+        $em = $this->get('doctrine')->getEntityManager();
+
+        //1er temps supression du content de la table etudiant
+
+        /** mettre ca dans la boucle d'insertion **/
+        //$note = new Note();
+        //$em->persist($note);
+        //$em->flush();
+
+        return true;
     }
 
-    private function importModules(){
+    /**
+    * @param string
+    *
+    * @return boolean
+    */
+    private function importModules($path){
     	//elminaoui todo here
+
+        $em = $this->get('doctrine')->getEntityManager();
+
+        //1er temps supression du content de la table etudiant
+
+        /** mettre ca dans la boucle d'insertion **/
+        //$module = new Module();
+        //$em->persist($module);
+        //$em->flush();
+
+
+        return true;
     }
 
-    private function importElements(){
+    /**
+    * @param string
+    *
+    * @return boolean
+    */
+    private function importElements($path){
     	//el mehdi el hajri todo here
+
+        $em = $this->get('doctrine')->getEntityManager();
+
+        //1er temps supression du content de la table etudiant
+
+        /** mettre ca dans la boucle d'insertion **/
+        //$element = new Element();
+        //$em->persist($element);
+        //$em->flush();
+
+
+        return true;
     }
 
 }
---
+++ src/App/Bundle/BackOfficeBundle/Form/ImportFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface as FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface as OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\File;
+
+/**
+* 
+*/
+class ImportFormType extends AbstractType
+{
+	
+	public function buildForm(FormBuilderInterface $builder, $options = array()){
+
+		$builder->add("table","choice",array(
+										'empty_value' => 'chosir un la table charger',
+										'multiple' => false,
+										'preferred_choices' => array('etudiant'),
+										'choices' => array('etudiant' => 'Etudiant', 'filiere' => 'Filiere', 'module' => 'Module', 'note' => 'Note'),
+										'constraints' => array(
+											new NotBlank(
+												array('message' => "errors.import.table")
+												),
+											new Choice(
+													array(
+														'choices' => array('etudiant','note','module','filiere'),
+														'message' => 'errors.import.table',
+														)
+													),
+										),
+										));
+
+		$builder->add('attachement','file',array(
+											'constraints' => array(
+												new File(
+													array(
+														'uploadErrorMesssage' => 'errors.import.attachment',
+														'mimeTypesMesssage' => 'errors.import.attachment.mimeTypes',
+														'mimeTypes' => array('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel', 'text/csv'),
+													)
+												)
+											)
+											));
+
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver){
+		$resolver->setDefaults(array(
+			'csrf_protection' => true;
+			'csrf_field_name' => '_protection';
+			'intention' => 'data_exchange';
+			));
+	}
+
+	public function getName(){
+		return 'data_exchange';
+	}
+
+	
+}
