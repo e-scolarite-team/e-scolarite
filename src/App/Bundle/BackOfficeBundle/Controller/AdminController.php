893dd1034f11ecb3668646a6c2bb710d8f4059c9
menounga <paul.mbilong@gmail.com>
2014-03-19 02:00:37 +0000

    fini

--- src/App/Bundle/BackOfficeBundle/Controller/AdminController.php
+++ src/App/Bundle/BackOfficeBundle/Controller/AdminController.php
@@ -6,46 +6,277 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use App\Bundle\BackOfficeBundle\Entity\Admin;
-use App\Bundle\BackOfficeBundle\Form\TypeDemandeType;
+use App\Bundle\BackOfficeBundle\Form\AdminType;
 
 /**
- * TypeDemande controller.
+ * Admin controller.
  *
  */
-class TypeDemandeController extends Controller
+class AdminController extends Controller
 {
 
     /**
-     * Lists all Admin .
+     * Lists all Admin entities.
      *
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('AppBackOfficeBundle:TypeDemande')->findAll();
+        $entities = $em->getRepository('AppBackOfficeBundle:Admin')->findAll();
 
-        return $this->render('AppBackOfficeBundle:TypeDemande:index.html.twig', array(
+        return $this->render('AppBackOfficeBundle:Admin:index.html.twig', array(
             'entities' => $entities,
         ));
     }
-   
-
     /**
-     * Lists create Admin .
+     * Creates a new Admin entity.
      *
      */
-    public function createAdminAction()
+    public function createAction(Request $request)
+    {
+        $entity = new Admin();
+
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+        $errors = array();
+
+        if ($request->isMethod('POST')) 
+        {
+            $validator = $this->get("validator");
+            
+            $translator  = $this->get('translator');
+
+            $errList = $validator->validate($form);        
+
+            if(count($errList) > 0)
+            {
+                foreach ($errList as $err) 
+                   $errors[] =  $translator->trans($err->getMessage(),array(), 'messages', 'fr_FR');  
+            }
+            else
+            {
+                $em = $this->getDoctrine()->getManager();
+                $entity->setCreatedAt(new \DateTime());
+                $entity->setSalt("");
+                $em->persist($entity);
+                $em->flush();
+                return $this->redirect($this->generateUrl('admin', array('id' => $entity->getId())));
+            }
+        }
+
+        return $this->render('AppBackOfficeBundle:Admin:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+            'errors' => $errors,
+        ));
+    }
+
+    /**
+    * Creates a form to create a Admin entity.
+    *
+    * @param Admin $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createCreateForm(Admin $entity)
+    {
+        $form = $this->createForm(new AdminType(), $entity, array(
+            'action' => $this->generateUrl('admin_create'),
+            'method' => 'POST',
+        ));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Admin entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Admin();
+        $form   = $this->createCreateForm($entity);
+        $errors = array();
+
+
+        return $this->render('AppBackOfficeBundle:Admin:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+             'errors'   => $errors,
+        ));
+    }
+
+    /**
+     * Finds and displays a Admin entity.
+     *
+     */
+    public function showAction($id)
     {
         $em = $this->getDoctrine()->getManager();
-        $admin= new Admin();
 
-        $this->createFormBuilder($admin)
-             ->add("nom","text")
-             ->add("prenom","text")
+        $entity = $em->getRepository('AppBackOfficeBundle:Admin')->find($id);
 
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Admin entity.');
+        }
 
+        $deleteForm = $this->createDeleteForm($id);
 
+        return $this->render('AppBackOfficeBundle:Admin:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
     }
-   
+
+    /**
+     * Displays a form to edit an existing Admin entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBackOfficeBundle:Admin')->find($id);
+        $errors = array();
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Admin entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AppBackOfficeBundle:Admin:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+            'errors' => $errors,
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Admin entity.
+    *
+    * @param Admin $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Admin $entity)
+    {
+        $form = $this->createForm(new AdminType(), $entity, array(
+            'action' => $this->generateUrl('admin_update', array('id' => $entity->getId())),
+            'method' => 'POST',
+        ));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Admin entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBackOfficeBundle:Admin')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Admin entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+         $errors = array();
+
+        if ($request->isMethod('POST')) 
+        {
+
+            $validator = $this->get("validator");
+            
+            $translator  = $this->get('translator');
+
+            $errList = $validator->validate($editForm);        
+
+            if(count($errList) > 0){
+            
+                foreach ($errList as $err) 
+                   $errors[] =  $translator->trans($err->getMessage(),array(), 'messages', 'fr_FR');
+                
+            }else{
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($entity);
+                $em->flush();
+                return $this->redirect($this->generateUrl('admin', array('id' => $id)));
+            }
+        }
+
+      
+        return $this->render('AppBackOfficeBundle:Admin:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Admin entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AppBackOfficeBundle:Admin')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Admin entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin'));
+    }
+
+    /**
+     * Deletes a Admin entity.
+     *
+     */
+    public function deleteGetAction(Request $request, $id)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AppBackOfficeBundle:Admin')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Admin entity.');
+        }
+
+        $em->remove($entity);
+        $em->flush();
+        
+
+        return $this->redirect($this->generateUrl('admin'));
+    }
+
+    /**
+     * Creates a form to delete a Admin entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
 }
